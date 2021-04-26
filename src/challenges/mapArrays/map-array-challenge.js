9fd83277b4536318ce74248bb06fef6570614e4e
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-26 16:02:02 -0700

    String It Up

--- src/challenges/mapArrays/map-array-challenge.js
+++ src/challenges/mapArrays/map-array-challenge.js
@@ -3,3 +3,9 @@ export function doubleNumbers(arr) {
   const doubled = numbers.map(item => item * 2);
   return (doubled);
 }
+
+export function stringItUp(arr) {
+  const numbers = [2, 5, 100];
+  const stringy = numbers.map(item => item + '');
+  return (stringy);
+}
--- src/challenges/mapArrays/map-array-challenge.test.js
+++ src/challenges/mapArrays/map-array-challenge.test.js
@@ -1,7 +1,13 @@
-import { doubleNumbers } from './map-array-challenge';
+import { doubleNumbers, stringItUp } from './map-array-challenge';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
   const output = doubleNumbers(input); // act
   expect(output).toEqual([4, 10, 200]); // assert
+});
+
+test('string it up', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = stringItUp(input); // act
+  expect(output).toEqual(['2', '5', '100']); // assert
 });
