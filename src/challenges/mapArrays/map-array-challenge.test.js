06a8e97e55d86c44cad6c57058f8397570cf2372
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-26 17:07:40 -0700

    a

--- src/challenges/mapArrays/map-array-challenge.test.js
+++ src/challenges/mapArrays/map-array-challenge.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable quotes */
-import { doubleNumbers, stringItUp, capitalizeNames, namesOnly } from './map-array-challenge';
+import { doubleNumbers, stringItUp, capitalizeNames, namesOnly, makeStrings, readyToPutInTheDOM } from './map-array-challenge';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
@@ -49,5 +49,71 @@ test('names Only', () => {
     "Paris Hilton",
     "Kayne West",
     "Bob Ziroll"
+  ]);
+});
+
+test.only('make strings', () => {
+  const input = [
+    {
+      name: "Angelina Jolie",
+      age: 80
+    },
+    {
+      name: "Eric Jones",
+      age: 2
+    },
+    {
+      name: "Paris Hilton",
+      age: 5
+    },
+    {
+      name: "Kayne West",
+      age: 16
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100
+    }
+  ];
+  const output = makeStrings(input);
+  expect(output).toEqual([
+    "Angelina Jolie can go to The Matrix",
+    "Eric Jones is under age!!",
+    "Paris Hilton is under age!!",
+    "Kayne West is under age!!",
+    "Bob Ziroll can go to The Matrix"
+  ]);
+});
+
+test('make headers', () => {
+  const input = [
+    {
+      name: "Angelina Jolie",
+      age: 80
+    },
+    {
+      name: "Eric Jones",
+      age: 2
+    },
+    {
+      name: "Paris Hilton",
+      age: 5
+    },
+    {
+      name: "Kayne West",
+      age: 16
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100
+    }
+  ];
+  const output = readyToPutInTheDOM(input);
+  expect(output).toEqual([
+    "<h1>Angelina Jolie</h1><h2>80</h2>",
+    "<h1>Eric Jones</h1><h2>2</h2>",
+    "<h1>Paris Hilton</h1><h2>5</h2>",
+    "<h1>Kayne West</h1><h2>16</h2>",
+    "<h1>Bob Ziroll</h1><h2>100</h2>"
   ]);
 });
