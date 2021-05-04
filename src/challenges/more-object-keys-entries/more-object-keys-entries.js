6a4442c0f0b9619f60dbc6cffd48952f7fe403a9
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-04 15:26:14 -0700

    Add Tests

---
+++ src/challenges/more-object-keys-entries/more-object-keys-entries.js
@@ -0,0 +1,14 @@
+export function getHouses(characters) {
+}
+
+export function updateNumbers(obj) {
+}
+
+export function totalCharacters(arr) {
+}
+
+export function hasChildrenEntries(arr, character) {
+}
+
+export function sortByChildren(arr) {
+}
