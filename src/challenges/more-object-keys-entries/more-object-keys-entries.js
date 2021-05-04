ea48345f6a72466b84f1f36e098e9ae74cce519d
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-04 16:56:15 -0700

    update numbers passes

--- src/challenges/more-object-keys-entries/more-object-keys-entries.js
+++ src/challenges/more-object-keys-entries/more-object-keys-entries.js
@@ -4,6 +4,12 @@ export function getHouses(obj) {
 }
 
 export function updateNumbers(obj) {
+  const arr = [];
+  Object.keys(obj).forEach(key => {
+    arr.push(`${key}: ${obj[key]}`);
+  });
+  return arr;
+
 }
 
 export function totalCharacters(arr) {
