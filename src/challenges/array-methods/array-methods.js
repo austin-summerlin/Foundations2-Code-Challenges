92ee3ecdc1aeadab39746246b0e21b69cad04f33
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-09 13:57:34 -0700

    Add Tests

---
+++ src/challenges/array-methods/array-methods.js
@@ -0,0 +1,32 @@
+export function howMuchPencil(str) {
+
+}
+
+export function wordsToCharList(str) {
+
+}
+
+export function listFoods(recipe) {
+
+}
+
+export function stepActions(recipe) {
+
+}
+
+export function removeLastCharacters(str, numberOfCharacters) {
+
+}
+
+// stretch goals 
+export function totalSumCSV(str) {
+
+}
+
+export function removeVowels(str) {
+
+}
+
+export function extractVowels(str) {
+
+}
