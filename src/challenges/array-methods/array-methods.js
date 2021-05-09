7dc48873322744bda30b74a73fe0336a02d19013
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-09 15:04:46 -0700

    Tests Working

--- src/challenges/array-methods/array-methods.js
+++ src/challenges/array-methods/array-methods.js
@@ -1,32 +1,42 @@
+//Write a function name howMuchPencil that takes in a string, as written on the side of a pencil.
+//As you sharpen the pencil, the string will become shorter and shorter, starting by removing the first letter.
+//Your function should use slice within a loop and return an array of each successice string result from losing letters to the sharpener, until nothing is left.
+//For example, if the input is 'Welcome', the output will be ['Welcome', 'elcome', 'lcome', 'come', 'ome', 'om', 'e']
 export function howMuchPencil(str) {
+  const arr = [];
+  for (let i = 0; i <= str.length; i++) {
+    arr.push(str.substring(i, str.length));
+  }
+  return arr;
 
 }
 
+//Write a function name wordsToCharList that, given a string as inputm returns a new array where every element is a character of the input string.
+//For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r']
 export function wordsToCharList(str) {
-
+  return str.split('');
 }
 
+//You are making a grocery list for ingredients needed in the gruffalo crumble recipe, below. Rather than taking the entire recipt, you only want a list of the item names.
+// Write a function named listFoods that takes in the recipe and returns a new arrat o the food items without any amount or units. Just the name. For example, '1 cup flour' wild return 'flour'.
 export function listFoods(recipe) {
-
+  const arr = recipe.ingredients.map(n => n.split(' ').splice(2).join(' '));
+  return arr;
 }
 
+//Use the same recipe (gruffaloCrumble) from challenge 3, above.
+//Write a function named stepAction that takes in the recipe and extracts the action verbs from the steps. Fortunate for you, the action verbs are the first word of each action.
+//Return a new array containing just the verbs. For example, ['Mix until evenly distrubuted'] returns ['Mix'].
 export function stepActions(recipe) {
-
+  const arr = recipe.steps.map(n => n.split(' ').splice(0, 1)[0]);
+  return arr;
 }
 
+//Write a function name removeLastCharacters that takes in a string and a number. The numberOfCharacters argument determines how many characters will be removed from the end of the string.
+//Return the resulting string.
+//If the numberOfCharacters argument is greater that the length of the input string, the function should return an empty string.
+//If the numberOfCharacters argument is a negative number, the function should return the input string without any changes.
+//For example removeLastCharacters('Gregor', 2) returns 'Greg', removeLastCharacters('Gregor', -2) returns 'Gregor', removeLastCharacters('Gregor', 9) returns ''.
 export function removeLastCharacters(str, numberOfCharacters) {
-
-}
-
-// stretch goals 
-export function totalSumCSV(str) {
-
-}
-
-export function removeVowels(str) {
-
-}
-
-export function extractVowels(str) {
-
+  return str.substring(0, str.length - numberOfCharacters);
 }
--- src/challenges/array-methods/array-methods.test.js
+++ src/challenges/array-methods/array-methods.test.js
@@ -1,6 +1,5 @@
-import { howMuchPencil, listFoods, removeLastCharacters, stepActions, wordsToCharList, totalSumCSV, removeVowels, extractVowels } from './array-methods.js';
+import { howMuchPencil, wordsToCharList, listFoods, stepActions, removeLastCharacters } from './array-methods';
 
-// data for testing
 const gruffaloCrumble = {
   name: 'How to make a Gruffalo Crumble',
   ingredients: [
@@ -27,53 +26,39 @@ const gruffaloCrumble = {
     'Spread the crisp evenly over the gruffalo mixture',
     'Bake for 12-15 hours',
   ]
-}; 
+};
 
-// the tests
-test('howMuchPencil should return a list of shortening words', () => {
+test('It should return a list of shortening words', () => {
   expect(howMuchPencil('Welcome')).toStrictEqual(['Welcome', 'elcome', 'lcome', 'come', 'ome', 'me', 'e', '']);
   expect(howMuchPencil('Welcome').length).toStrictEqual(8);
   expect(howMuchPencil('')).toStrictEqual(['']);
   expect(howMuchPencil('abc')).toStrictEqual(['abc', 'bc', 'c', '']);
 });
 
-test('wordsToCharList should return a list of characters', () => {
+
+test('It should return a set of characters', () => {
   expect(wordsToCharList('Gregor')).toStrictEqual(['G', 'r', 'e', 'g', 'o', 'r']);
   expect(wordsToCharList('Gregor').length).toStrictEqual(6);
   expect(wordsToCharList('hooray')).toStrictEqual(['h', 'o', 'o', 'r', 'a', 'y']);
   expect(wordsToCharList('')).toStrictEqual([]);
 });
-[6:10 PM]
-test('listFoods should return array of ingredients without their amounts', () => {
+
+
+test('It should return a list of items', () => {
   expect(listFoods(gruffaloCrumble)).toStrictEqual(['Gruffalo', 'oats', 'brown sugar', 'flour', 'pure maple syrup', 'chopped nuts', 'baking soda', 'baking powder', 'cinnamon', 'melted butter', 'fresh water']);
+
 });
 
-test('stepActions should return array of actions for each step', () => {
+test('It should return step actions', () => {
   expect(stepActions(gruffaloCrumble)).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']);
+
 });
 
-test('removeLastCharacters', () => {
+test('It should remove last characters', () => {
   expect(removeLastCharacters('Gregor', 2)).toStrictEqual('Greg');
   expect(removeLastCharacters('hello', -1)).toStrictEqual('hello');
   expect(removeLastCharacters('wowow', -700)).toStrictEqual('wowow');
   expect(removeLastCharacters('hello', 12)).toStrictEqual('');
   expect(removeLastCharacters('', 1)).toStrictEqual('');
   expect(removeLastCharacters('a', 1)).toStrictEqual('');
-});
-
-// stretch goals
-test('totalSumCSV', () => {
-  expect(totalSumCSV('1,4,5,7,2')).toStrictEqual(19);
-  expect(totalSumCSV('147')).toStrictEqual(147);
-});
-
-test('removeVowels', () => {
-  expect(removeVowels('gregor')).toStrictEqual('grgr');
-  expect(removeVowels('asdf')).toStrictEqual('sdf');
-  expect(removeVowels('why')).toStrictEqual('why');
-});
-
-test('extractVowels', () => {
-  expect(extractVowels('gregor')).toStrictEqual(['grgr', 'eo']);
-  expect(extractVowels('The quick brown fox')).toStrictEqual(['Th qck brwn fx', 'eioou']);
 });
